4fa4b75da8da7405e673ab078e83fba02bcaac67
zzzzz <2659294465@qq.com>
2023-09-22 21:40:17 +0800

    new pojo - stu

---
+++ src/main/java/com/web/pojo/stu.java
@@ -0,0 +1,30 @@
+package com.web.pojo;
+
+public class stu {
+    String name;
+    String address;
+
+    public stu() {
+    }
+
+    public stu(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
